45a4e5e710fba9b287c392b6e3e7d433f442a00f
Dangomimikyu <54048830+Dangomimikyu@users.noreply.github.com>
2021-11-23 10:59:55 +0800

    tried to fade in but not working yet

--- Assets/Scripts/Gameplay/Quest/QuestEndUIHandler.cs
+++ Assets/Scripts/Gameplay/Quest/QuestEndUIHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DangoMimikyu.EventManagement;
+using DG.Tweening;
 
 public class QuestEndUIHandler : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class QuestEndUIHandler : MonoBehaviour
     #region Monobehaviour functions
     void Start()
     {
+        m_rewardPanel.gameObject.SetActive(false); // turn off the quest overlay
         EventManager.instance.StartListening(GameEvents.Gameplay_QuestEnd, ToggleRewardPanel);
     }
 
@@ -31,6 +33,7 @@ public class QuestEndUIHandler : MonoBehaviour
 	private void ToggleRewardPanel(EventArgumentData ead)
 	{
         m_rewardPanel.gameObject.SetActive(!m_rewardPanel.gameObject.activeSelf);
+        m_rewardPanel.DOFade(1.0f, 1.0f).SetEase(Ease.InBounce);
 	}
     #endregion
 }
